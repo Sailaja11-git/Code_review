66425f5d3da9565b4fa973111169a81385a2fddd
Sailaja Karani (EXT) <sailaja.karani.ext@ericsson.com>
2025-07-24 12:50:21 +0530

    Updated codefile.py for review

--- codefile.py
+++ codefile.py
@@ -70,7 +70,7 @@ def write_report(output_path, content):
 
 
 def main():
-    input_file = "/C:/Users/zrkasia/Desktop/Code_Review_agent/input.py"
+    input_file = r"C:\Users\zrkasia\Desktop\Code_Review_agent\input"
 
     # Read code from the input file
     try:
